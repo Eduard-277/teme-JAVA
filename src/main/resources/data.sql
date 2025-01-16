3cecf5ef9d15add026ba5dde669214aaef82b2c1
Eduard Denis Bran <168176270+Eduard-277@users.noreply.github.com>
2025-01-16 18:15:48 +0200

    Add files via upload

---
+++ src/main/resources/data.sql
@@ -0,0 +1,7 @@
+-- drop table if exists evenimente;
+-- create table evenimente(id INT primary key AUTO_INCREMENT, titlul varchar(50) not null, autorul varchar(50) not null);
+
+insert into evenimente(denumire,locatie,data,ora,pret) values ("Concert Phoenix","M2","2024-04-19","20:30",100);
+insert into evenimente(denumire,locatie,data,ora,pret) values ("Concert Cargo","Sala Olimpia","2024-03-18","19:00",40);
+insert into evenimente(denumire,locatie,data,ora,pret) values ("O noapte furtunoasa","Teatrul National","2023-09-25","19:00",60);
+insert into evenimente(denumire,locatie,data,ora,pret) values ("O scrisoare pierduta","Teatrul National","2023-08-20","19:00",50);
